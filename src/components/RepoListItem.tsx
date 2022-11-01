294537d5debfdc59b32407f5a382964b1f035535
navivix <gkaczmarek2304@gmail.com>
2022-11-01 14:16:29 +0100

    Fix linter warnings

--- src/components/RepoListItem.tsx
+++ src/components/RepoListItem.tsx
@@ -1,6 +1,5 @@
 import { ListItemButton, ListItemText } from "@mui/material";
 import Link from "./Link";
-import type { Repo } from "../types";
 
 interface RepoListItemProps {
   name: string;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -19,7 +19,7 @@ export default function SearchBar(props: SearchBarProps) {
   };
 
   const handleKeyPress = (event: any) => {
-    if (event.keyCode == 13) {
+    if (event.keyCode === 13) {
       props.onSearch();
     }
   };
